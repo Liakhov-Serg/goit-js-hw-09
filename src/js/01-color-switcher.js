9b7fbb5cfb0f6ef9e472577a920014a31cfcf722
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-06 18:25:47 +0200

    Update 01-color-switcher.js
    Good

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,34 +2,60 @@
 // import getRandomHexColor from './randomHexColor';
 
 //Get button start/stop element
-const dataStartRef = document.querySelector('[data-start]');
-const dataStopRef = document.querySelector('[data-stop]');
-const bodyRef = document.querySelector('body');
-// values
-let timerId = null;
 
-// Add event listener start/stop on btn start/stop
-dataStartRef.addEventListener('click', onStart);
-dataStopRef.addEventListener('click', onStop);
+const refs = {
+  startBtn:document.querySelector(`button[data-start]`),
+  stopBtn:document.querySelector(`button[data-stop]`),
+};
+let timerId;
+console.log(refs);
 
-// Function start event listener
-function onStart() {
-  timerId = setInterval(getBgColor, 1000);
-
-  dataStartRef.toggleAttribute('disabled');
-}
-
-// Function stop event listener
-function onStop() {
+refs.startBtn.addEventListener('click', () => {
+  refs.stopBtn.disabled = false;
+  refs.startBtn.disabled = true;
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+});
+refs.stopBtn.disabled = true;
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
   clearInterval(timerId);
+});
 
-  dataStartRef.removeAttribute('disabled');
-}
+// const dataStartRef = document.querySelector('[data-start]');
+// const dataStopRef = document.querySelector('[data-stop]');
+// const bodyRef = document.querySelector('body');
+// // values
+// let timerId = null;
 
-// Function set random bg color
-function getBgColor() {
-  bodyRef.style.backgroundColor = getRandomHexColor();
+// // Add event listener start/stop on btn start/stop
+// dataStartRef.addEventListener('click', onStart);
+// dataStopRef.addEventListener('click', onStop);
+
+// // Function start event listener
+// function onStart() {
+//   timerId = setInterval(getBgColor, 1000);
+
+//   dataStartRef.toggleAttribute('disabled');
+// }
+
+// // Function stop event listener
+// function onStop() {
+//   clearInterval(timerId);
+
+//   dataStartRef.removeAttribute('disabled');
+// }
+
+// // Function set random bg color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// function getBgColor() {
+//   bodyRef.style.backgroundColor = getRandomHexColor();
+// }
 
 
 
