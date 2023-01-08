6b1efc8dee2db29f74820ae1cedd2e70423d5ba9
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-08 11:04:04 +0200

    Update 03-promises.js
    with Repeta

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,25 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.3;
+
+  setTimeout(() => {
+  if (canFulfill) {
+    resolve(`Проміс виконався успішно, с результатом (виконан, fulfilled)`);
+  }
+  reject(`Проміс виконався з помилкою (відхилено, rejected)`);
+}, 2000);
+  
+});
+// Виконали планіровку - відкладені виклики
+promise.then(
+  result => {
+  console.log(result);
+},
+error => {
+  console.log(error);
+},
+);
+
+console.log(promise);
