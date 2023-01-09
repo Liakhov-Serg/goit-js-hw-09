8e519c044c40eed7b95c4d871506927ef03f6eb4
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-10 00:46:57 +0200

    00-46
    111

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,25 +6,51 @@ function createPromise(position, delay) {
     // Reject
   }
 }
-const promise = new Promise((resolve, reject) => {
-  const canFulfill = Math.random() > 0.3;
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.3;
 
-  setTimeout(() => {
-  if (canFulfill) {
-    resolve(`Проміс виконався успішно, с результатом (виконан, fulfilled)`);
-  }
-  reject(`Проміс виконався з помилкою (відхилено, rejected)`);
-}, 2000);
+//   setTimeout(() => {
+//   if (canFulfill) {
+//     resolve(`Проміс виконався успішно, с результатом (виконан, fulfilled)`);
+//   }
+//   reject(`Проміс виконався з помилкою (відхилено, rejected)`);
+// }, 2000);
   
-});
-// Виконали планіровку - відкладені виклики
-promise.then(
-  result => {
-  console.log(result);
-},
-error => {
-  console.log(error);
-},
-);
+// });
+// // Виконали планіровку - відкладені виклики
+// promise
+// .then(onFulfilled)
+// .then(x => {
+//   console.log(x);
+//   return 10;
+// })
+// .then(y => {
+//   console.log(y);
+// })
+// .catch(error => console.log(error))
+// .finally(() => console.log(`В кожному випадку`));
+  
 
-console.log(promise);
+
+// console.log(promise);
+
+// function onFulfilled(result) {
+//   console.log(`onFulfilled -> onFulfilled`);
+// }
+
+const makeOrder = dish => {
+  
+  return Promise.resolve(`Ось ваше замовлення`);
+      }
+     
+
+makeOrder(`пиріжок`).then(onMakeOrderSuccess).catch(onMakeOrderError);
+
+function onMakeOrderSuccess(result) {
+  console.log(`onMakeOrderSuccess`);
+  console.log(result);
+}
+function onMakeOrderError(error) {
+  console.log(`onMakeOrderError`);
+  console.log(error);
+}
