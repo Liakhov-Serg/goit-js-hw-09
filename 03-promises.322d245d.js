25fbe5960b4047db0b4f2d1bde2847a68f2c5e12
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-09 22:47:22 +0000

    Deploying to gh-pages from @ Liakhov-Serg/goit-js-hw-09@8e519c044c40eed7b95c4d871506927ef03f6eb4 🚀

---
+++ 03-promises.322d245d.js
@@ -0,0 +1,2 @@
+Promise.resolve("Ось ваше замовлення").then((function(o){console.log("onMakeOrderSuccess"),console.log(o)})).catch((function(o){console.log("onMakeOrderError"),console.log(o)}));
+//# sourceMappingURL=03-promises.322d245d.js.map
